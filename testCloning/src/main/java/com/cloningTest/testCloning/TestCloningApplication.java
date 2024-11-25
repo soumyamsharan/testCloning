a82f67376d98f2c4e46076027f712741a1694bc1
Soumyam Sharan <83439401+soumyamsharan@users.noreply.github.com>
2024-11-25 22:26:35 +0530

    Add files via upload

---
+++ testCloning/src/main/java/com/cloningTest/testCloning/TestCloningApplication.java
@@ -0,0 +1,13 @@
+package com.cloningTest.testCloning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCloningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCloningApplication.class, args);
+	}
+
+}
---
+++ testCloning/src/test/java/com/cloningTest/testCloning/TestCloningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloningTest.testCloning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCloningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
